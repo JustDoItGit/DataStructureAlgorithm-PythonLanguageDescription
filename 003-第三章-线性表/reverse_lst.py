6c290163b2b51eedc7894f710537d8dc027cd82d
coolcumt <cool_kj@126.com>
2018-12-21 11:03:17 +0800

    列表反转直到两个下标碰头时操作完成

--- 003-第三章-线性表/reverse_lst.py
+++ 003-第三章-线性表/reverse_lst.py
@@ -1,7 +1,7 @@
 def reverse(lst):
     elems = lst
     i, j = 0, len(elems) - 1
-    while i < j:
+    while i < j:  # 直到两个下标碰头时操作完成
         elems[i], elems[j] = elems[j], elems[i]
         i, j = i + 1, j - 1
     return elems
