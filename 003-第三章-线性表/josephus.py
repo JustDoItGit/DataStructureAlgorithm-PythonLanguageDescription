4f996ca99ba2e4f5ef543b731170b4ae179c6289
coolcumt <cool_kj@126.com>
2019-01-04 10:22:03 +0800

    约瑟夫环基于顺序表的解O(n^2)、约瑟夫环基于单链表的解O(n) + O(m*n) = O((m+1)*n)

--- 003-第三章-线性表/josephus.py
+++ 003-第三章-线性表/josephus.py
@@ -1,4 +1,8 @@
+from single_cycle_link_objectList import LCList
+
+
 def josephus_a(n, k, m):
+    """ 约瑟夫环基于数组的解O(n^2*log(n)) """
     people = list(range(1, n + 1))
     i = k - 1
     for num in range(n):
@@ -17,5 +21,36 @@ def josephus_a(n, k, m):
     return
 
 
+def josephus_l(n, k, m):
+    """ 约瑟夫环基于顺序表的解O(n^2) """
+    people = list(range(1, n + 1))
+
+    num, i = n, k - 1
+    for num in range(n, 0, -1):
+        i = (i + m - 1) % num
+        print(people.pop(i), end=(', ' if num > 1 else '\n'))
+
+    return
+
+
+class Josephus(LCList):
+    """ 约瑟夫环基于单链表的解O(n) + O(m*n) = O((m+1)*n)"""
+
+    def turn(self, m):
+        for i in range(m):
+            self._rear = self._rear.next_
+
+    def __init__(self, n, k, m):
+        super().__init__()
+        for i in range(n):
+            self.append(i + 1)
+        self.turn(k - 1)
+        while not self.is_empty():
+            self.turn(m - 1)
+            print(self.pop(), end='\n' if self.is_empty() else ', ')
+
+
 if __name__ == '__main__':
     josephus_a(10, 2, 7)
+    josephus_l(10, 2, 7)
+    Josephus(10, 2, 7)
